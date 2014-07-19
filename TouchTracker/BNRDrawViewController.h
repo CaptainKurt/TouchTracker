397b20339a1efb13c59d689a1e3a7204872baae6
CaptainKurt <walkkg12@wfu.edu>
2014-07-18 21:58:48 -0400

    Added Necessary Classes

---
+++ TouchTracker/BNRDrawViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRDrawViewController.h
+//  TouchTracker
+//
+//  Created by Kurt Walker on 7/18/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRDrawViewController : UIViewController
+
+@end
---
+++ TouchTracker/BNRLine.m
@@ -0,0 +1,13 @@
+//
+//  BNRLine.m
+//  TouchTracker
+//
+//  Created by Kurt Walker on 7/18/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRLine.h"
+
+@implementation BNRLine
+
+@end
