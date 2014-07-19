6c3ec82ffe5c08ccddc2596df300f9fda7d02f3d
CaptainKurt <walkkg12@wfu.edu>
2014-07-19 15:30:35 -0400

    Added gestures
    Added gesture recognizers:
    Double tap: erase all lines
    Singe tap: select a single line

--- TouchTracker/BNRDrawView.m
+++ TouchTracker/BNRDrawView.m
@@ -13,6 +13,9 @@
 
 @property (nonatomic, strong) NSMutableDictionary *linesInProgress;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
+@property (nonatomic, weak) UIColor *lineColor;
+
+@property (nonatomic, weak) BNRLine *selectedLine;
 
 @end
 
@@ -27,6 +30,20 @@
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+        
+        UITapGestureRecognizer *doubleTapRecognizer =
+            [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                    action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTapRecognizer];
+        
+        UITapGestureRecognizer *tapRecognizer =
+            [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                    action:@selector(tap:)];
+        tapRecognizer.delaysTouchesBegan = YES;
+        [tapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
+        [self addGestureRecognizer:tapRecognizer];
     }
     
     return self;
@@ -40,6 +57,10 @@
     
     [bp moveToPoint:line.begin];
     [bp addLineToPoint:line.end];
+    self.lineColor = [self selectLineColorFromAngleValue:[self angleFromStartToEndingPoints:line
+                                                                                 startPoint:line.begin
+                                                                                   endPoint:line.end]];
+    [self.lineColor setStroke];
     [bp stroke];
 }
 
@@ -54,6 +75,11 @@
     [[UIColor redColor] set];
     for (NSValue *key in self.linesInProgress) {
         [self strokeLine:self.linesInProgress[key]];
+    }
+    
+    if (self.selectedLine) {
+        [[UIColor greenColor] set];
+        [self strokeLine:self.selectedLine];
     }
 }
 
@@ -122,5 +148,103 @@
     }
 }
 
+- (int)angleFromStartToEndingPoints:(BNRLine *)line
+                         startPoint:(CGPoint)begin
+                           endPoint:(CGPoint)end {
+    return (((atan2((line.end.x - line.begin.x) , (line.end.y - line.begin.y)))*180)/M_PI);
+}
 
+- (void)doubleTap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized Double Tap");
+    
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
+}
+
+- (void)tap:(UIGestureRecognizer *)gr
+{
+    NSLog(@"Recognized tap");
+    
+    CGPoint point = [gr locationInView:self];
+    self.selectedLine = [self lineAtPoint:point];
+    
+    if (self.selectedLine) {
+        // Make ourselves the target of menu item action messages
+        [self becomeFirstResponder];
+        
+        // Grab the menu controller
+        UIMenuController *menu = [UIMenuController sharedMenuController];
+        
+        // Create a new "Delete" UIMenuItem
+        UIMenuItem *deleteItem = [[UIMenuItem alloc] initWithTitle:@"Delete"
+                                                            action:@selector(deleteLine:)];
+        menu.menuItems = @[deleteItem];
+        
+        // Tell the menu where it should come from and show it
+        [menu setTargetRect:CGRectMake(point.x, point.y, 2, 2) inView:self];
+        [menu setMenuVisible:YES animated:YES];
+    }
+    else {
+        // Hide the menu if no line is selected
+        [[UIMenuController sharedMenuController] setMenuVisible:NO animated:YES];
+    }
+    
+    [self setNeedsDisplay];
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)deleteLine:(id)sender
+{
+    // Remove the selected line from the list of finishedLines
+    [self.finishedLines removeObject:self.selectedLine];
+    
+    // Redraw
+    [self setNeedsDisplay];
+}
+
+
+- (BNRLine *)lineAtPoint:(CGPoint)p
+{
+    // Find a line close to p
+    for (BNRLine *l in self.finishedLines) {
+        CGPoint start = l.begin;
+        CGPoint end = l.end;
+        
+        // Check a few points on the line
+        for (float t = 0.0; t <= 1.0; t += 0.05) {
+            float x = start.x + t * (end.x - start.x);
+            float y = start.y + t * (end.y - start.y);
+            
+            // If the tapped point is within 20 ppoints, lets return this line
+            if (hypot(x - p.x, y - p.y) < 20) {
+                return l;
+            }
+        }
+    }
+    
+    // If nothing is close enough to the tapped point, then we did not select a line
+    return nil;
+}
+
+- (UIColor *)selectLineColorFromAngleValue:(int)angleValue {
+    UIColor *lc = nil;
+    if (angleValue >= 0 && angleValue <= 90) {
+        lc = [UIColor redColor];
+    } else if (angleValue >= 91 && angleValue <= 180) {
+        lc = [UIColor blueColor];
+    } else if (angleValue < 0 && angleValue >= -90) {
+        lc = [UIColor greenColor];
+    } else if (angleValue < -90 && angleValue >= -179) {
+        lc = [UIColor yellowColor];
+    } else {
+        lc = [UIColor blackColor];
+    }
+    return lc;
+}
 @end
